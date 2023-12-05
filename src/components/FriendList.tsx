bcafc7dede236e486df299e017b05a1e66adf5e4
shubham <49603826+shubhdevelop@users.noreply.github.com>
2023-12-05 19:18:19 +0530

    [Feature] Add Emoji picker

--- src/components/FriendList.tsx
+++ src/components/FriendList.tsx
@@ -2,7 +2,7 @@ import Friend from "./Friend";
 
 function FriendList() {
   return (
-    <div className=" py-4 col-span-3 overflow-y-auto h-full  scrollbar-thin scrollbar-thumb-gray-400 scrollbar-track-gray-100">
+    <div className=" py-4 col-span-3 overflow-y-auto h-full border-r-2 scrollbar-thin scrollbar-thumb-gray-400 scrollbar-track-gray-100">
       <Friend
         name={"Shubham Raj"}
         unread="hiiiii"
@@ -13,81 +13,6 @@ function FriendList() {
         unread="hiiiii"
         profilePic="https://github.com/shadcn.png"
       />{" "}
-      <Friend
-        name={"Shubham Raj"}
-        unread="hiiiii"
-        profilePic="https://github.com/shadcn.png"
-      />{" "}
-      <Friend
-        name={"Shubham Raj"}
-        unread="hiiiii"
-        profilePic="https://github.com/shadcn.png"
-      />{" "}
-      <Friend
-        name={"Shubham Raj"}
-        unread="hiiiii"
-        profilePic="https://github.com/shadcn.png"
-      />
-      <Friend
-        name={"Shubham Raj"}
-        unread="hiiiii"
-        profilePic="https://github.com/shadcn.png"
-      />
-      <Friend
-        name={"Shubham Raj"}
-        unread="hiiiii"
-        profilePic="https://github.com/shadcn.png"
-      />
-      <Friend
-        name={"Shubham Raj"}
-        unread="hiiiii"
-        profilePic="https://github.com/shadcn.png"
-      />
-      <Friend
-        name={"Shubham Raj"}
-        unread="hiiiii"
-        profilePic="https://github.com/shadcn.png"
-      />
-      <Friend
-        name={"Shubham Raj"}
-        unread="hiiiii"
-        profilePic="https://github.com/shadcn.png"
-      />
-      <Friend
-        name={"Shubham Raj"}
-        unread="hiiiii"
-        profilePic="https://github.com/shadcn.png"
-      />
-      <Friend
-        name={"Shubham Raj"}
-        unread="hiiiii"
-        profilePic="https://github.com/shadcn.png"
-      />
-      <Friend
-        name={"Shubham Raj"}
-        unread="hiiiii"
-        profilePic="https://github.com/shadcn.png"
-      />
-      <Friend
-        name={"Shubham Raj"}
-        unread="hiiiii"
-        profilePic="https://github.com/shadcn.png"
-      />
-      <Friend
-        name={"Shubham Raj"}
-        unread="hiiiii"
-        profilePic="https://github.com/shadcn.png"
-      />
-      <Friend
-        name={"Shubham Raj"}
-        unread="hiiiii"
-        profilePic="https://github.com/shadcn.png"
-      />
-      <Friend
-        name={"Shubham Raj"}
-        unread="hiiiii"
-        profilePic="https://github.com/shadcn.png"
-      />
     </div>
   );
 }
