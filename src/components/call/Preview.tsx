c8d01755a48b8e192cfe4a798744a79b4bdc079f
shubham <49603826+shubhdevelop@users.noreply.github.com>
2023-12-13 01:51:48 +0530

    [Fix] videoTrack not switching when userMuted
    fixed the bug where user where not able to turn off
    their camera when there mic was off, as navigator.mediaDevices.getUserMedia,
    requires atleast one of teh video or audio to be true

--- src/components/call/Preview.tsx
+++ src/components/call/Preview.tsx
@@ -9,50 +9,47 @@ const Preview = ({ isCalling, isVideo, isMuted }: Props) => {
   const videoRef = useRef<HTMLVideoElement | null>(null);
   console.log(`video ${isVideo}, muted: ${isMuted}`);
 
-  useEffect(() => {
-    const startWebcam = async () => {
-      try {
-        const stream = await navigator.mediaDevices.getUserMedia({
-          video: isVideo,
-          audio: !isMuted,
-        });
+  const startWebcam = async () => {
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia({
+        video: isVideo,
+        audio: true,
+      });
 
-        if (videoRef.current && stream) {
+      if (videoRef.current && stream) {
+        if (isMuted && isVideo) {
+          const VideoTracks = stream.getVideoTracks();
+          if (VideoTracks.length > 0) {
+            const videoStreamOnly = new MediaStream(VideoTracks);
+            videoRef.current.srcObject = videoStreamOnly;
+          }
+        } else {
           videoRef.current.srcObject = stream as MediaStream;
         }
-      } catch (error) {
-        console.error("Error accessing webcam:", error);
       }
-    };
+    } catch (error) {
+      console.error("Error accessing webcam:", error);
+    }
+  };
 
-    const capture = () => {
-      if (videoRef.current) {
-        const canvas = document.createElement("canvas");
-        canvas.width = videoRef.current.videoWidth;
-        canvas.height = videoRef.current.videoHeight;
+  const capture = () => {
+    if (videoRef.current) {
+      const canvas = document.createElement("canvas");
+      canvas.width = videoRef.current.videoWidth;
+      canvas.height = videoRef.current.videoHeight;
 
-        const context = canvas.getContext("2d");
-        if (context) {
-          if (isMuted) {
-            context.drawImage(videoRef.current, 0, 0, 1000, canvas.height);
-          }
-          context.drawImage(
-            videoRef.current,
-            0,
-            0,
-            canvas.width,
-            canvas.height
-          );
+      const context = canvas.getContext("2d");
+      if (context) {
+        context.drawImage(videoRef.current, 0, 0, canvas.width, canvas.height);
 
-          const imageSrc = canvas.toDataURL("image/jpeg");
-          // Now you can use the captured image source as needed
-          console.log(imageSrc);
-        }
+        const imageSrc = canvas.toDataURL("image/jpeg");
+        // Now you can use the captured image source as needed
+        console.log(imageSrc);
       }
-    };
-    startWebcam();
-    capture();
-  }, [isVideo]);
+    }
+  };
+  startWebcam();
+  capture();
 
   const styleWhenCalling = `absolute bottom-4 drop-shadow-xl right-4 w-[300px]`;
 
@@ -67,7 +64,7 @@ const Preview = ({ isCalling, isVideo, isMuted }: Props) => {
           style={{
             transform: "rotateY(180deg)",
           }}
-          muted={true}
+          muted={false}
           ref={videoRef}
           width={1000}
           height={360}
